fe35aefe9ffd60847e0f7f094dbeab06e28cc579
arjun <arjun16tomar@gmail.com>
2024-09-28 23:33:52 +0530

    Implemented Edit Button, used .update() method

--- gui.py
+++ gui.py
@@ -1,12 +1,16 @@
 import functions
 import FreeSimpleGUI as sg
 
-label = sg.Text("Type in  a to-do")                 # A Text Label
+label = sg.Text("Type in  a to-do")                         # A Text Label
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
-add_button = sg.Button("Add", bind_return_key=True)                       # A Button
+list_display = sg.Listbox(values=functions.get_todos(), key="list_todos",
+                          enable_events=True, size=[45,10])
+add_button = sg.Button("Add")                               # A Button
+edit_button = sg.Button("Edit")
 
 layout = [ [label],                                 # The Layout of Window
-           [input_box, add_button]                  # Each item in separate row is shown in different row
+           [input_box, add_button],                  # Each item in separate row is shown in different row
+           [list_display, edit_button]
          ]                                          # input box and button show on single row
 
 window = sg.Window("To-Do App",                     # Window Definition
@@ -19,9 +23,24 @@ while True:
     print(values)                                               # returns the user input
     match event:
         case "Add":
-            todos = functions.get_todos()
-            todos.append(values["todo"] + "\n")
-            functions.write_todos(todos)
+            todos_list = functions.get_todos()
+            todos_list.append(values["todo"] + "\n")
+            functions.write_todos(todos_list)
+            window["list_todos"].update(values=todos_list)
+            window["todo"].update("")
+
+        case "Edit":
+            todo_to_edit = values["list_todos"][0]
+            new_todo = values["todo"]
+
+            todos_list = functions.get_todos()
+            index = todos_list.index(todo_to_edit)
+            todos_list[index] = new_todo + "\n"
+            functions.write_todos(todos_list)
+            window["list_todos"].update(values=todos_list)
+
+        case "list_todos":
+            window["todo"].update(value=values['list_todos'][0])
 
         case sg.WIN_CLOSED:
             break
